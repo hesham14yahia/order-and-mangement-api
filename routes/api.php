2c302de31cf2665efb77180a66157d980a7698f7
hesham14yahia <hesham14yahia@gmail.com>
2026-01-28 10:36:21 +0200

    organize endpoints

--- routes/api.php
+++ routes/api.php
@@ -6,23 +6,22 @@ use App\Http\Controllers\Api\V1\OrderController;
 use App\Http\Controllers\Api\V1\PaymentController;
 
 Route::group(["prefix" => "v1"], function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-
     Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-
         Route::group(['prefix' => 'orders'], function () {
             Route::post('confirm/{order}', [OrderController::class,'confirm']);
             Route::post('cancel/{order}', [OrderController::class,'cancel']);
         });
         Route::apiResource('orders', OrderController::class);
 
-
         Route::group(['prefix' => 'payments'], function () {
             Route::get('/', [PaymentController::class,'index']);
             Route::get('/{order}/', [PaymentController::class,'show']);
             Route::post('charge/{order}', [PaymentController::class,'charge']);
         });
+
+        Route::post('/logout', [AuthController::class, 'logout']);
     });
+
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 });
